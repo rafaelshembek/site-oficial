e219e99581c36ea63b9af1a4a5dc41ec5465f016
Rafael Silva <rafaelshembek@gmail.com>
2024-02-03 20:23:49 -0300

    pequenas modificações na pagina layout

--- app/layout.js
+++ app/layout.js
@@ -2,21 +2,23 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Inicial from "../public/components/Home/page";
 import Footer from "../public/components/Footer/page";
-import Helmet_Head from "./helmet";
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Rafael Silva Dev',
   description: 'Desenvolvedor Web e Mobile',
+  keywords: "Developer, Web, App, Reactjs, React Native, NextJs, Desenvolvedor Web, Desenvolverdo React Native, Rafael Silva, Dev",
+  robots: "index, follow"
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <Helmet_Head></Helmet_Head>
       <body className={inter.className}>
         <Inicial></Inicial>
         {children}
+        <Analytics />
         {/* <Footer></Footer> */}
       </body>
     </html>
